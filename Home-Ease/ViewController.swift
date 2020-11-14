176eb2f27792c07b3f86612f313c1cfd96d9627d
dhruvpatel1999 <dhruvpatel@wustl.edu>
2020-11-14 17:03:54 -0600

    Initial Commit

---
+++ Home-Ease/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Home-Ease
+//
+//  Created by Dhruv Patel on 11/14/20.
+//  Copyright © 2020 Dhruv Patel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

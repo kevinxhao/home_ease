7189d1bfab5dd006b4e062c13bb5e517759d3d32
JTidland <j.tidland@wustl.edu>
2020-11-29 01:02:14 -0600

    added functionality to task view controller
    I added a detailed view controller, which is pushed onto the nav controller whenever this week or next week are clicked

--- Home-Ease/TasksViewController.swift
+++ Home-Ease/TasksViewController.swift
@@ -8,19 +8,45 @@
 
 import UIKit
 
-class TasksViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class TasksViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var taskCollectionView: UICollectionView!
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return 2;
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        <#code#>
+        let myCell = taskCollectionView.dequeueReusableCell(withReuseIdentifier: "TaskCollectionViewCell", for: indexPath) as! TaskCollectionViewCell
+        var myCellFrame = CGRect()
+        if(indexPath.row == 0){
+            myCellFrame = CGRect(x: view.frame.minX, y: view.frame.minY, width: view.frame.width, height: taskCollectionView.frame.height/2 - 10)
+            myCell.weekLabel.text = "This Week"
+        }
+        else if(indexPath.row == 1){
+            myCellFrame = CGRect(x: view.frame.minX, y: taskCollectionView.frame.height/2 + 10, width: view.frame.width, height: taskCollectionView.frame.height/2 - 10)
+            myCell.weekLabel.text = "Next Week"
+        }
+        myCell.frame = myCellFrame
+        myCell.backgroundColor = .blue
+        
+        return myCell;
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let listTasksVC = storyboard?.instantiateViewController(withIdentifier: "listTask") as! listOfTasksViewController
+        navigationController?.pushViewController(listTasksVC, animated: true)
+    }
+    
+    func setUpCollectionView(){
+        taskCollectionView.dataSource = self
+        taskCollectionView.delegate = self
     }
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpCollectionView()
         // Do any additional setup after loading the view.
     }
     
